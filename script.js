a1bb42f9a25bb89d4e333a554a14e6f6e8b84233
Matteo S <sciascia.matteo74@gmail.com>
2023-11-15 06:50:58 +0100

    feat removed stars background, added nav bar and some content

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const smallStars = document.getElementById('smallStars');
 const mediumStars = document.getElementById('mediumStars');
 const bigStars = document.getElementById('bigStars');
@@ -121,3 +122,81 @@ function triggerShootingStar() {
         star.style.display = 'block'; // Make the star visible
     }, delay);
 }
+/*
+
+/*=============== SHOW MENU ===============*/
+const headerToggle = document.getElementById('header-toggle'),
+      main = document.getElementById('main'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(headerToggle){
+    headerToggle.addEventListener('click', () =>{
+        main.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        main.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+const contentSections = document.querySelectorAll('.content')
+
+/*
+function linkAction(){
+    const main = document.getElementById('main')
+    // When we click on each nav__link, we remove the show-menu class
+    main.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+*/
+navLink.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        const targetLink = event.target;
+        console.log(targetLink);
+        const activeContentSection = document.getElementsByClassName('content active')[0];
+        const targetContentSection = document.getElementById(targetLink.href.split('#')[1] + "-content");
+        if (activeContentSection !== targetContentSection) {
+            const activeLink = document.getElementsByClassName('nav__link active-link')[0];
+            activeLink.classList.remove('active-link')
+            targetLink.classList.add('active-link');
+            activeContentSection.classList.remove('active');
+            targetContentSection.classList.add('active');
+        }
+    })
+})
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
